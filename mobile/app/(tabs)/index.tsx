132969f697d4f5115474082570a4f76d7f8d3271
Mustafa Pakdil <mpakdil0@gmail.com>
2026-05-13 22:54:19 +0300

    style: adjust bid badge position to be lower on the card

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -3002,7 +3002,7 @@ const styles = StyleSheet.create({
   },
   bidStatPremiumBadge: {
     position: 'absolute',
-    bottom: 6,
+    bottom: 0,
     right: 0,
     flexDirection: 'row',
     alignItems: 'center',
